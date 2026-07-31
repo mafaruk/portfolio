f850af41d614a4358628d2e8d6588cd1f354a726
Mafaruk Bagwan <bagwannihal4@gmail.com>
2026-08-01 04:21:01 +0530

    further SSR chngs

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -3,18 +3,35 @@ import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 const server = require('../../dist/server/server.js').default;
 
+function getRequestUrl(event) {
+  const protocol = event.headers['x-forwarded-proto'] || 'https';
+  const host = event.headers['x-forwarded-host'] || event.headers.host || 'localhost';
+  const path = event.path || '/';
+  return `${protocol}://${host}${path}`;
+}
+
+function getHeaders(event) {
+  return Object.entries(event.headers || {}).reduce((acc, [key, value]) => {
+    if (typeof value === 'string') acc[key] = value;
+    return acc;
+  }, {});
+}
+
 export const handler = async (event) => {
-  const request = new Request(`https://${event.headers.host || 'localhost'}${event.path || '/'}`, {
+  const request = new Request(getRequestUrl(event), {
     method: event.httpMethod || 'GET',
-    headers: event.headers,
-    body: event.body,
+    headers: getHeaders(event),
+    body: event.body || undefined,
   });
 
   const response = await server.fetch(request, {}, {});
 
+  const headers = Object.fromEntries(response.headers.entries());
+  const body = await response.text();
+
   return {
     statusCode: response.status,
-    headers: Object.fromEntries(response.headers.entries()),
-    body: await response.text(),
+    headers,
+    body,
   };
 };
